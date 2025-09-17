50e05b12aa3fc89a1a855f1bb08df09dd01100a2
Chowhound <825352674@qq.com>
2025-09-17 16:51:29 +0800

    fix 批量存图

--- src/main/java/per/chowh/bot/plugins/img/ImagesMsgStorage.java
+++ src/main/java/per/chowh/bot/plugins/img/ImagesMsgStorage.java
@@ -1,13 +1,8 @@
 package per.chowh.bot.plugins.img;
 
 import cn.hutool.http.HttpUtil;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.mikuac.shiro.dto.action.common.ActionData;
-import com.mikuac.shiro.dto.action.response.GetForwardMsgResp;
-import com.mikuac.shiro.dto.action.response.MsgResp;
 import com.mikuac.shiro.dto.event.message.PrivateMessageEvent;
 import com.mikuac.shiro.enums.MsgTypeEnum;
-import com.mikuac.shiro.model.ArrayMsg;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -16,25 +11,20 @@ import per.chowh.bot.core.registery.annotation.EventListener;
 import per.chowh.bot.core.utils.FileInfo;
 import per.chowh.bot.core.utils.MsgUtils;
 import per.chowh.bot.ext.filter.annotation.EventFilter;
-import per.chowh.bot.utils.JacksonUtil;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-import java.util.regex.Matcher;
 
 @Slf4j
 @Component
 public class ImagesMsgStorage {
     @Value("${per.file-location}")
     private String IMAGE_PATH;
-    @Value("${per.video-location-raw}")
-    private String VIDEO_PATH_RAW;
     @Value("${per.video-location}")
     private String VIDEO_PATH;
     private final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
@@ -45,47 +35,18 @@ public class ImagesMsgStorage {
         List<FileInfo> fileInfos = MsgUtils.getImageUrlFromMsg(bot, event, true);
 
         fileInfos.forEach(imageInfo -> {
-            if (imageInfo.isVideo()) {
-                saveVideo(imageInfo.getUrl(), imageInfo.getFileName());
-            }else {
-                saveImage(imageInfo.getUrl(), imageInfo.getFileName());
-            }
+            String desPath = imageInfo.isVideo() ? VIDEO_PATH : IMAGE_PATH;
+            HttpUtil.downloadFile(imageInfo.getUrl(), getPathWithDate(desPath) + File.separatorChar + imageInfo.getFileName());
         });
     }
 
 
-    private void saveVideo(String url, String fileName) {
-        String substring = url.substring(VIDEO_PATH_RAW.length());
 
-        try {
-            Files.copy(Path.of(VIDEO_PATH + File.separatorChar + substring), Path.of(getImagePath() + File.separatorChar + fileName));
-            log.info("video fileName: {} --- url:{}", fileName, url);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-    private void saveMedia(JsonNode node) {
-        for (JsonNode jsonNode : node) {
-            for (JsonNode message : jsonNode.get("message")) {
-                JsonNode data = message.get("data");
-                if ("image".equals(message.get("type").asText())) {
-                    saveImage(data.get("url").asText(), data.get("file").asText());
-                } else if ("video".equals(message.get("type").asText())) {
-                    saveVideo(data.get("url").asText(), data.get("file").asText());
-                } else if (message.get("type").asText().equals("forward")) {
-                    saveMedia(data.get("content"));
-                }
-            }
-        }
-    }
-    private void saveImage(String url, String fileName) {
-        HttpUtil.downloadFile(url, getImagePath() + File.separatorChar + fileName);
-        log.info("image: fileName: {} --- url:{}", fileName, url);
-    }
 
-    private String getImagePath() {
+
+    private String getPathWithDate(String basePath) {
         String format = dateFormat.format(new Date());
-        Path path = Path.of(IMAGE_PATH + File.separatorChar + format);
+        Path path = Path.of(basePath + File.separatorChar + format);
         try {
             if (!Files.exists(path)) {
                 Files.createDirectory(path);
